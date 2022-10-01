256762d61a2d40b810e6c74c9cb2191cf1c0d7e8
Ode Dahay <odedahay@Odes-MacBook-Pro.local>
2022-10-01 15:24:10 +0800

    fixing html

--- pages/about.js
+++ pages/about.js
@@ -5,8 +5,8 @@ export default function AboutPage() {
     <Layout title='About Dash Off Blog'>
         <h1 className="text-5xl border-b-4 pb-5 font-bold">ABOUT ME</h1>
         <div className='bg-white shadow-md rounded-lg px-10 py-6 mt-6'>
-        <h3 className='text-2xl mb-5'>Hi, there! I'm Rodel and my friends called me 'Ode'</h3>
-          <img src="/images/odedahay-logo.jpeg" width="720px" alt="Evolution of logo" />
+        <h3 className='text-2xl mb-5'>{" Hi, there! I'm Rodel and my friends called me 'Ode'"}</h3>
+          <img src='/images/odedahay-logo.jpeg' width='720px' alt='Evolution of logo' />
           <p>I'm a UX/UI designer and seasonal developer. I have a passion for creating websites and love to developing interactive designs</p>
           <p>I simply love and enjoy my 24/7 activities:</p>
           <ul class="list-disc pl-5">
