9d8fd5f872853e8b1b4e6d135bb164926a3e2a59
Ode Dahay <odedahay@Odes-MacBook-Pro.local>
2022-10-01 15:27:11 +0800

    fixing html

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import Layout from "@/components/Layout"
+import Image from "next/image"
 
 export default function AboutPage() {
   return (
@@ -6,7 +7,7 @@ export default function AboutPage() {
         <h1 className="text-5xl border-b-4 pb-5 font-bold">ABOUT ME</h1>
         <div className='bg-white shadow-md rounded-lg px-10 py-6 mt-6'>
         <h3 className='text-2xl mb-5'>{" Hi, there! I'm Rodel and my friends called me 'Ode'"}</h3>
-          <img src='/images/odedahay-logo.jpeg' width='720px' alt='Evolution of logo' />
+          <Image src='/images/odedahay-logo.jpeg' width={600} height={408} alt='Evolution of logo' />
           <p>I'm a UX/UI designer and seasonal developer. I have a passion for creating websites and love to developing interactive designs</p>
           <p>I simply love and enjoy my 24/7 activities:</p>
           <ul class="list-disc pl-5">
